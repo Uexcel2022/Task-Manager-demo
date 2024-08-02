5f7d0700c45c81ae689a9433af9ae1897971715d
Uexcel2022 <excellenceudoka@gmail.com>
2024-08-02 02:25:53 +0100

    project created

---
+++ src/main/java/uexcel/com/taskmanagerdemo/TaskManagerDemoApplication.java
@@ -0,0 +1,13 @@
+package uexcel.com.taskmanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerDemoApplication.class, args);
+	}
+
+}
